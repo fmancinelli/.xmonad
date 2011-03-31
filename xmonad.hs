65f48e0b4c654aa56cc00a556df0db4d9fd5e749
Fabio Mancinelli <fm@fabiomancinelli.org>
2011-03-31 17:47:28 +0200

    Added gimp workspace and layout. Refactored existing code.

--- xmonad.hs
+++ xmonad.hs
@@ -22,6 +22,7 @@ devWorkspace = "dev"
 webWorkspace = "web"
 termWorkspace = "term"
 docWorkspace = "doc"
+gimpWorkspace = "gimp"
 scratchWorkspace = "scratch"
 imWorkspace = "IM" 
 
@@ -29,7 +30,7 @@ imWorkspace = "IM"
 main = do
     statusBarPipe <- spawnPipe "xmobar $HOME/.xmonad/xmobarrc"
     xmonad $ defaultConfig
-        { workspaces = [devWorkspace, webWorkspace, termWorkspace, docWorkspace, scratchWorkspace, imWorkspace],          
+        { workspaces = [devWorkspace, webWorkspace, termWorkspace, docWorkspace, gimpWorkspace, scratchWorkspace, imWorkspace],          
           manageHook = myManageHooks <+> manageHook defaultConfig,          
           layoutHook = myLayouts,
           logHook = dynamicLogWithPP $ myXmobarPP statusBarPipe,
@@ -45,13 +46,17 @@ main = do
 -- Windows organization
 myManageHooks = composeAll
   [
+    (role =? "gimp-toolbox" <||> role =? "gimp-image-window") --> (ask >>= doF . W.sink),
     className =? "Chromium-browser" --> viewShift webWorkspace,
     className =? "Skype" --> viewShift imWorkspace,
     className =? "Pidgin" --> viewShift imWorkspace,
     className =? "Eclipse" --> viewShift devWorkspace,
+    className =? "Gimp" --> viewShift gimpWorkspace,
     manageDocks
   ]
-  where viewShift = doF . liftM2 (.) W.greedyView W.shift
+  where
+    viewShift = doF . liftM2 (.) W.greedyView W.shift
+    role = stringProperty "WM_WINDOW_ROLE"
         
 -- Xmobar Pretty Printer configuration
 myXmobarPP pipe = xmobarPP {
@@ -65,21 +70,20 @@ myXmobarPP pipe = xmobarPP {
 }
 
 -- Layout configuration
-myTabbedLayout = tabbed shrinkText defaultTheme {
-  fontName = "xft:Ubuntu:pixelsize=11"
-}
-  
-myIMLayout = reflectHoriz $ withIM (0.20) (Role "buddy_list") $ withIM (0.20) (Role "MainWindow") (Grid ||| Full)
- 
 myLayouts = avoidStruts $ 
-            onWorkspace webWorkspace myTabbedLayout $
-            onWorkspace imWorkspace myIMLayout $
+            onWorkspace webWorkspace tabbedLayout $
+            onWorkspace imWorkspace imLayout $
+            onWorkspace gimpWorkspace gimpLayout $
             standardLayouts
           where 
+            tabbedLayout = tabbed shrinkText defaultTheme {
+                                               fontName = "xft:Ubuntu:pixelsize=11"
+                                             }
+            imLayout = reflectHoriz $ withIM (0.20) (Role "buddy_list") $ withIM (0.20) (Role "MainWindow") (Grid ||| Full)
+            gimpLayout = withIM (0.11) (Role "gimp-toolbox") $ reflectHoriz $ withIM (0.15) (Role "gimp-dock") (Full ||| Grid)
             standardLayouts = tiled ||| Mirror tiled ||| Full
             tiled = ResizableTall 1 (3/100) (1/2) []
             
-
 -- Dynamic shell prompt configuration
 myPromptConfig = defaultXPConfig {
   font = "xft:Ubuntu:pixelsize=11",
